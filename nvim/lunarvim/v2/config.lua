134085efffb92620dafab05ba6eed2fffabb7267
austin <austinchang4@gmail.com>
2022-05-15 22:49:49 +0800

    lunarvim: install indent blankline plugin

--- nvim/lunarvim/v2/config.lua
+++ nvim/lunarvim/v2/config.lua
@@ -171,6 +171,18 @@ lvim.plugins = {
         "folke/trouble.nvim",
         cmd = "TroubleToggle",
     },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "BufRead",
+        setup = function()
+            vim.g.indentLine_enabled = 1
+            vim.g.indent_blankline_char = "▏"
+            vim.g.indent_blankline_filetype_exclude = { "help", "terminal", "dashboard" }
+            vim.g.indent_blankline_buftype_exclude = { "terminal" }
+            vim.g.indent_blankline_show_trailing_blankline_indent = false
+            vim.g.indent_blankline_show_first_indent_level = false
+        end
+    },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
