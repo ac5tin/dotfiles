07fbf10c1122cef9d70e79bb8546b70bca974506
austin <austinchang4@gmail.com>
2022-07-09 03:30:57 +0800

    feat: wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm';
+return {
+    font = wezterm.font("JetBrains Mono"),
+    font_size = 10.0,
+    font_antialias = "Subpixel",
+    color_scheme = "Blue Matrix",
+    window_background_opacity = 0.8,
+}
