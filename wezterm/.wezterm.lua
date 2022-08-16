571f6fc9c98838a93db90430bec86031d552d07d
austin <austinchang4@gmail.com>
2022-08-17 00:47:07 +0800

    feat(wezterm): keybinds for switching tabs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,4 +5,31 @@ return {
     font_antialias = "Subpixel",
     color_scheme = "Blue Matrix",
     window_background_opacity = 0.8,
+    keys = {
+        {
+            key = "x",
+            mods = "CTRL|SHIFT",
+            action = "ActivateCopyMode",
+        },
+        {
+            key = "v",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ PasteFrom = "Clipboard" }),
+        },
+        {
+            key = "c",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }),
+        },
+        {
+            key = "LeftArrow",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.ActivateTabRelative(-1)
+        },
+        {
+            key = "RightArrow",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.ActivateTabRelative(1)
+        }
+    }
 }
