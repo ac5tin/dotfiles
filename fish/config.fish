3fabb894d9ca684fb9a54d15e49ca055c81c96cb
austin <austinchang4@gmail.com>
2023-08-29 11:42:17 +0800

    feat(fish): include starship

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,34 @@
+# disable fish greeter
+function fish_greeting
+end
+
+
 set -gx PATH $PATH ~/bin
+set -gx PATH $PATH ~/.local/bin
 set -gx PATH $PATH ~/.cargo/bin
 set -gx PATH $PATH ~/.yarn/bin
 set -gx PATH $PATH ~/go/bin
+set -gx PATH $PATH ~/.linkerd2/bin
 
 set -gx LESS -Ri
 
-set -gx VISUAL nvim
+set -gx VISUAL lvim
 
-alias clinotes="less ~/Documents/clinotes/cli-commands.txt"
+alias clinotes="bat -p ~/Documents/clinotes/cli-commands.txt"
 alias eclinotes="lvim ~/Documents/clinotes/cli-commands.txt"
-alias othnotes="less ~/MEGA/notes/others.txt"
 
 alias tmux="env TERM=screen-256color tmux"
 alias ls="lsd"
-# get github token
-# - gh_token|xsel -bi
+alias less="bat -p"
+
+# github token
+# - gh_token |xsel -bi
 alias gh_token="bw get item github |jq -r '.fields[]|select(.name == \"token\")|.value'"
-alias jira="jira-terminal"
+
+# nnn
+set -gx NNN_PLUG 'f:finder;o:fzopen;p:mocplay;d:diffs;v:imgview'
 
 
-function jira-done
-    jira-terminal transition -t $argv done
-end
 
-function jira-assign
-    jira-terminal assign -t $argv[1] -u $argv[2]
-end
+# Starship (must be at the end)
+starship init fish | source
